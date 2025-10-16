8ac0e75021e1141707786246bd6bc8a7c29ae20c
xanstered <gzak2@edu.cdv.pl>
2025-10-16 19:48:35 +0200

    c++ init

---
+++ Source/UnrealGame/Public/AMasterGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AMasterGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALGAME_API AAMasterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
